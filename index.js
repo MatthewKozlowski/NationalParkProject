121419d61f275dfeef0789e215291944d9c3dd28
Matt <maekozlowski@gmail.com>
2019-04-08 15:23:30 -0600

    Added template code to modify

---
+++ index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+// put your own value below!
+const apiKey = 'fHYkY33fQmkk1Mz1kwTB1tbSkvIbgFY89C9J1atn'; 
+const searchURL = 'https://www.developer.nps.gov/api/v1';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+/*
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the items array
+  for (let i = 0; i < responseJson.items.length; i++){
+    // for each video object in the items 
+    //array, add a list item to the results 
+    //list with the video title, description,
+    //and thumbnail
+    $('#results-list').append(
+      `<li><h3>${responseJson.items[i].snippet.title}</h3>
+      <p>${responseJson.items[i].snippet.description}</p>
+      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+*/
+function displayResults() {
+    console.log(data);
+}
+function getNationalParks(query, maxResults=10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    maxResults,
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getNationalParks(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
