edc385d19a9f0a4c36e7fa08cedb673236af84ae
Matthew Kozlowski <maekozlowski@gmail.com>
2019-04-09 20:41:47 -0600

    redid js

--- index.js
+++ index.js
@@ -1,69 +1,27 @@
 'use strict';
 
-// put your own value below!
-const apiKey = 'fHYkY33fQmkk1Mz1kwTB1tbSkvIbgFY89C9J1atn'; 
-const searchURL = 'https://www.developer.nps.gov/api/v1';
+const apiKey = 'gGH1CiZxsC2auy7GhfhoGQhadcPEfA1ikB8KyV9j'
+const searchUrl = 'https://developer.nps.gov/api/v1/parks'
 
-
-function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
-}
-/*
-function displayResults(responseJson) {
-  console.log(responseJson);
-  $('#results-list').empty();
-  // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++){
-    // for each video object in the items 
-    //array, add a list item to the results 
-    //list with the video title, description,
-    //and thumbnail
-    $('#results-list').append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
-      </li>`
-    )};
-  //display the results section  
-  $('#results').removeClass('hidden');
-};
-*/
-function displayResults() {
-    console.log(data);
-}
-function getNationalParks(query, maxResults=10) {
+function getNationalParks(query, maxResults){
   const params = {
     key: apiKey,
     q: query,
-    maxResults,
-  };
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-
-  console.log(url);
-
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displayResults(responseJson))
-    .catch(err => {
-      $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
+    
+  }
 }
 
-function watchForm() {
-  $('form').submit(event => {
+function watchForm(){
+  $('form').submit(function(event){
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
+    let searchTerm = $('#js-search-term').val();
+    let maxResults = $('#js-max-results').val();
+    console.log(searchTerm+" "+maxResults);
     getNationalParks(searchTerm, maxResults);
-  });
+  })
 }
 
-$(watchForm);
+$(function(){
+  console.log('National Park Search App Loaded! Waiting for user input.');
+  watchForm();
+})
