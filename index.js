d44a3b82d7d15f7de4efcd5beed6b27a4271d236
Matthew Kozlowski <maekozlowski@gmail.com>
2019-04-16 16:06:31 -0600

    add JS

--- index.js
+++ index.js
@@ -2,20 +2,72 @@
 
 const apiKey = 'gGH1CiZxsC2auy7GhfhoGQhadcPEfA1ikB8KyV9j'
 const searchUrl = 'https://developer.nps.gov/api/v1/parks'
+let maxResults = 10;
 
-function getNationalParks(query, maxResults){
-  const params = {
-    key: apiKey,
-    q: query,
-    
+function displayNationalParks(responseJson){
+  console.log(responseJson);
+  $('ol').remove();
+  $("#results-list").append('<ol></ol>');
+  if(maxResults > responseJson.data.length){
+    for(let i = 0; i < responseJson.data.length; i++){
+      console.log(maxResults);
+      $('ol').append(`
+      <li>
+      <p>Park Name: ${responseJson.data[i].fullName}</p>
+      <p>Description: ${responseJson.data[i].description}</p>
+      <p>Website: <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</p>
+      </li>`);
+    }  
+  }else {
+    for(let i = 0; i < maxResults; i++){
+      console.log(maxResults);
+      $('ol').append(`
+      <li>
+      <p>Park Name: ${responseJson.data[i].fullName}</p>
+      <p>Description: ${responseJson.data[i].description}</p>
+      <p>Website: <a href="${responseJson.data[i].url}">${responseJson.data[1].url}</p>
+      </li>`);
+    }  
   }
 }
 
-function watchForm(){
-  $('form').submit(function(event){
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+  .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
+}
+
+function getNationalParks(query, maxResults){
+  const params = {
+    api_key: apiKey,
+    q: query,
+    maxResults
+  };
+
+  
+  const queryString = formatQueryParams(params)
+  const url = searchUrl + '?' + queryString;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(responseJson => displayNationalParks(responseJson))
+}
+
+function watchForm1(){
+  $('#js-form1').submit(function(event){
     event.preventDefault();
-    let searchTerm = $('#js-search-term').val();
-    let maxResults = $('#js-max-results').val();
+    let searchTerm = $('#js-search-term1').val();
+    maxResults = $('#js-max-results1').val();
+    console.log(searchTerm+" "+maxResults);
+    getNationalParks(searchTerm, maxResults);
+  })
+}
+
+function watchForm2(){
+  $('#js-form2').submit(function(event){
+    event.preventDefault();
+    let searchTerm = $('#js-search-term2').val();
+    maxResults = $('#js-max-results2').val();
     console.log(searchTerm+" "+maxResults);
     getNationalParks(searchTerm, maxResults);
   })
@@ -23,5 +75,6 @@ function watchForm(){
 
 $(function(){
   console.log('National Park Search App Loaded! Waiting for user input.');
-  watchForm();
+  watchForm1();
+  watchForm2();
 })
